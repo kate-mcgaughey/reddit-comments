218867a581bc7c6a76f15b9efa5ecee0b0edbf0b
McGaughey <therealmcgaughey@gmail.com>
2016-04-21 11:08:26 -0600

    I've had this open for 6 hours and honestly don't know what I last changed. Meh. There's like nothing here anyway.

--- index.php
+++ index.php
@@ -1,15 +1,46 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
-		<meta charset="UTF-8"
-				
-
-
-
-
+		<meta charset="UTF-8">
+		<title>Personas</title>
 	</head>
-
-
-
-
+	<body>
+		<header>
+			<!--For my instructors and classmates, I wanted the header to contain a link to the referenced site-->
+			<h1>Data Design Project - <a href="https://www.reddit.com/r/PHP/comments/4ff7bw/a_php_feature_born_from_a_tweet//">Reddit Comments</a></h1>
+		</header>
+		<main>
+			<h2>Part 1 - Personas</h2>
+			<img src="" alt="" align="right">
+			<p>Sara is a developer using the reddit community to learn and share knowledge. She responds to the post asking for assistance because she's on HHVM team. What is that thing about PHP intervals? I want to understand how to describe its relevance. Sara both votes on others' comments and contributes her own. She sometimes gets mentioned by other users for her thoughtful remarks. The thing that attracts Sara to this site most is that it's curated by the community</p>
+			<h2>Part 2 - Use Case</h2>
+			<ol>
+				<li>Sara logs into her account and checks to see if she has any messages, which she did not.</li>
+				<li>She locates her favorite subreddits and browses them for new links, questions, and stories.</li>
+				<li>Sara responds to a post about a PHP feature that was born from an idea expressed in someone's tweet.</li>
+				<li>She upvotes two other comments that respond to the same post.</li>
+				<li>She downvotes one other comment that responds to the same post.</li>
+				<li>She is mentioned by another user in a comment responding to the same post.</li>
+			</ol>
+		</main>
+	</body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
