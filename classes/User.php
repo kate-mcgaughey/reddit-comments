d7fe8a98c7a1bae71c5267ccd2dae16d05cad57a
Kate McGaughey <kmcgaughey@cnm.edu>
2016-04-21 20:41:45 -0600

    Constructor method

--- classes/User.php
+++ classes/User.php
@@ -19,7 +19,35 @@ class User implements \JsonSerializable {
 	private $userEmail;
 	/**
 
+	 /**
+	 * constructor for this User
 	 *
-	 */
+	 * @param int|null $newUserId of this User or null if a new User
+	 * @param string $newUserEmail string containing newUserEmail
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurrs
+	 **/
+
+	public function __construct(int $newUserId = null, string $newUserEmail) {
+		try {
+			$this->setUserId($newUserId);
+			$this->setUserEmail($newUserEmail);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			// rethrow the exception to the caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		}catch(\RangeException $range) {
+			// rethrow the exception to the caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		}catch(\TypeError $typeError) {
+			//rethrow the exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		}catch(\Exception $exception)	{
+			//rethrow the exception to the caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
+
 
 }
